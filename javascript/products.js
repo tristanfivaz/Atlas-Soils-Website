62480382837d5cd772445f9efdc0eb2ece011f3c
Tristan Fivaz <tristanfivaz04@gmail.com>
2024-10-22 19:42:51 +1000

    General updates to content in products page

--- javascript/products.js
+++ javascript/products.js
@@ -6,3 +6,13 @@ const navbarMenu = document.querySelector('.navbar-menu');
 menuToggle.addEventListener('click', () => {
     navbarMenu.classList.toggle('active');
 });
+
+// Image comparison slider functionality
+const container = document.querySelector('.comparison-image-container');
+const slider = document.querySelector('.slider');
+
+if (container && slider) { // Check if elements exist to avoid errors
+    slider.addEventListener('input', (e) => {
+        container.style.setProperty('--position', `${e.target.value}%`);
+    });
+}
